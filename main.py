5649093e84352102f31a2ab6d542837321945bfb
Ilya <kuzyahockey88@mail.ru>
2026-04-27 15:24:49 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from uuid import uuid4
+
+from fastapi import FastAPI, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=['*'],
+)
+
+
+class Task(BaseModel):
+    id: str
+    title: str
+    completed: bool
+
+class TaskCreate(BaseModel):
+    title: str
+
+
+tasks: list[Task] = []
+
+
+@app.get('/tasks', response_model=list[Task])
+def get_tasks():
+    return tasks 
+
+
+@app.post('/tasks', response_model=Task, status_code=status.HTTP_201_CREATED)
+def create_task(payload: TaskCreate):
+    task = Task(id=str(uuid4()), title=payload.title, completed=False)
+    tasks.append(task)
+    return task
+
